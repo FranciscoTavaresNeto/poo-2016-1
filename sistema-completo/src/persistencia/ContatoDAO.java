3d2d85d7c10e7b9967a3acf19da8b0f421752c18
marciojrtorres <marciojrtorres@gmail.com>
2016-08-01 14:20:10 -0300

    ultima revisao

--- sistema-completo/src/persistencia/ContatoDAO.java
+++ sistema-completo/src/persistencia/ContatoDAO.java
@@ -3,16 +3,47 @@ package persistencia;
 import dominio.Contato;
 
 import java.io.*;
+import java.sql.*;
 
 // Data Access Object (Objeto de Acesso a Dados)
 // em outras palavras: Persistência de Objetos
 public class ContatoDAO {
 
+	private static final String URL = 
+		"jdbc:postgresql://localhost:5432/agenda";
+	private static final String PWD = "postgres";
+	private static final String USR = "postgres";
+
 	public void salvar(Contato c) {
+		// banco
+		try {
+			// registrar o driver
+			Class.forName("org.postgresql.Driver");
+			// conexao
+			Connection conexao =
+			DriverManager.getConnection(URL, USR, PWD);
+			Statement comando = conexao.createStatement();
+			// TODO comando vulnerável a ataques de SQL Injection, MELHORAR DEPOIS!!!
+			String sql = "INSERT INTO contatos VALUES ('" 
+						+ c.getNome() + "', '" 
+						+ c.getTelefone() + "', '" 
+						+ c.getEmail() + "')";
+			// submete o comando para o banco
+			comando.execute(sql);
+			
+			conexao.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void salvarArquivo(Contato c) {
 		// TODO: fazer gravar em disco
 		try {
 			File arquivo = new File("contatos.csv");
-			PrintWriter printer = new PrintWriter(arquivo);
+			FileOutputStream saida =
+					new FileOutputStream(arquivo, true);
+			PrintWriter printer = new PrintWriter(saida);
 			printer.append(
 					c.getNome() + ";"
 			        + c.getTelefone() + ";"
@@ -25,3 +56,21 @@ public class ContatoDAO {
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
